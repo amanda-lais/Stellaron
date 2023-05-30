702fd94bee0e59bd5f5729fdf099505d47908731
amanda <100282290+amanda-lais@users.noreply.github.com>
2023-05-30 17:50:16 -0300

    Update grafoMatriz.py

--- Codigo-Projeto/grafoMatriz.py
+++ Codigo-Projeto/grafoMatriz.py
@@ -1,168 +1,8 @@
 import math
 from queue import PriorityQueue#############################
 
-
 # CLASSES
-# grafo matriz  --  rotulado ou não
 # grafo nao direcionado -- rotulado ou não
-# Implementar uma classe Vértice
-
-class GrafoMatriz:
-    TAM_MAX_DEFAULT = 100  # qtde de vértices máxima default
-    # construtor da classe grafo
-    def __init__(self, n=TAM_MAX_DEFAULT, rotulado=False):
-        self.n = n  # número de vértices
-        self.m = 0  # número de arestas
-        self.rotulado = False  # servirá de verificação nos métodos abaixo
-        # matriz de adjacência
-        if rotulado:
-            self.rotulado = True
-            self.adj: list[list[float]] = [[math.inf for i in range(n)] for j in range(n)]
-        else:
-            self.adj = [[0 for i in range(n)] for j in range(n)]
-
-    # Insere uma aresta no Grafo tal que
-    # v é adjacente a w
-    def insere_a(self, v, w, rotulo=-1):  # rotulo -1 é para quando não se sabe o peso daquela aresta
-        if self.rotulado and self.adj[v][w] == math.inf:
-            self.adj[v][w] = rotulo
-            self.m += 1
-        if not self.rotulado and self.adj[v][w] == 0:
-            self.adj[v][w] = 1
-            self.m += 1  # atualiza qtd arestas
-
-    def insere_v(self):
-        self.n += 1
-        if self.rotulado:
-            for linha in self.adj:
-                linha.append(math.inf)
-            self.adj.append([math.inf for i in range(self.n)])
-        else:
-            self.adj.append([0 for i in range(self.n)])
-
-    # remove uma aresta v->w do Grafo
-    def remove_a(self, v, w):
-        # testa se temos a aresta
-        if not self.rotulado and self.adj[v][w] == 1:
-            self.adj[v][w] = 0
-            self.m -= 1  # atualiza qtd arestas
-        if self.rotulado and self.adj[v][w] != math.inf:
-            self.adj[v][w] = math.inf
-            self.m -= 1
-
-    # Apresenta o Grafo contendo
-    # número de vértices, arestas
-    # e a matriz de adjacência obtida
-    def show(self):
-        print(f"\n n: {self.n:2d} m: {self.m:2d} r: {self.rotulado}\n")
-        for i in range(self.n):
-            for w in range(self.n):
-                print(f"Adj[{i:2d},{w:2d}] = {self.adj[i][w]} | ", end="")
-            print("\n")
-        print("\nfim da impressao do grafo.")
-
-    # Apresenta o Grafo contendo
-    # número de vértices, arestas
-    # e a matriz de adjacência obtida
-    # Apresentando apenas os valores 0 ou 1
-    def show_min(self):
-        print(f"\n n: {self.n:2d} m: {self.m:2d} r: {self.rotulado}\n")
-        for i in range(self.n):
-            for w in range(self.n):
-                print(f" {self.adj[i][w]} |", end="")
-            print("\n")
-        print("\nfim da impressao do grafo.")
-
-    def in_degree(self, v: int) -> int:
-        return len([linha for linha in self.adj if linha[v] != 0 and linha[v] != math.inf])
-
-    def out_degree(self, v: int) -> int:
-        return len([sai for sai in self.adj[v] if sai != 0 and sai != math.inf])
-
-    def is_fonte(self, v: int) -> int:
-        if self.in_degree(v) == 0 and self.out_degree(v) > 0:
-            return 1
-        return 0
-
-    def is_sorvedouro(self, v) -> int:
-        if self.in_degree(v) > 0 and self.out_degree(v) == 0:
-            return 1
-        return 0
-
-    def is_simetrico(self) -> int:
-        for i in range(len(self.adj)):
-            for j in range(len(self.adj[i])):
-                if self.adj[i][j] != self.adj[j][i]:
-                    return 0
-                else:
-                    return 1
-
-    def remover(self, v: int) -> int:
-        if v < self.n:
-            # Remove as arestas
-            for _ in range(0, len(self.adj[v])):
-                self.remove_a(v, _)
-                self.remove_a(_, v)
-            # Remove os vértices
-            for linha in self.adj:
-                del linha[v]
-            del self.adj[v]
-            self.n -= 1
-            return 1
-        else:
-            return 0
-
-    def completo(self) -> int:  # dei ctrl c ctrl v
-        checa = 1
-        for i in range(self.n):
-            for w in range(self.n):
-                if i != w:
-                    if self.adj[i][w] != 0 and self.adj[i][w] != math.inf:
-                        continue
-                    else:
-                        checa = 0
-                        break
-        if checa == 1:
-            return 1
-        else:
-            return 0
-
-    @staticmethod
-    def visitar_no(no: int):
-        print(f"Estamos visitando o nó {no}")
-
-    @staticmethod
-    def marcar_no(marcados, no):
-        marcados.append(no)
-        return marcados
-
-    def no_adjacente(self, no, marcados):
-        adjs = self.adj[no]
-        for _ in range(self.n):
-            if (adjs[_] != 0 and adjs[_] != math.inf) and _ not in marcados:
-                return _
-        return -1
-
-    def nos_adjacentes(self, no, marcados):
-        return [index for index, valor in enumerate(self.adj[no]) if (valor != 0 and valor != math.inf) and marcados]
-
-    def percurso_profundidade(self, v_inicio):
-        marcados = []
-        p = Pilha()
-        self.visitar_no(v_inicio)
-        marcados = self.marcar_no(marcados, v_inicio)
-        p.push(v_inicio)
-        while not p.is_empty():
-            no_atual = p.pop()
-            no_seguinte = self.no_adjacente(no_atual, marcados)
-            while no_seguinte != -1:
-                self.visitar_no(no_seguinte)
-                p.push(no_atual)
-                self.marcar_no(marcados, no_seguinte)
-                no_atual = no_seguinte
-                no_seguinte = self.no_adjacente(no_seguinte, marcados)
-
-
 
 class TGrafoND:
     TAM_MAX_DEFAULT = 100
@@ -294,25 +134,96 @@ class TGrafoND:
             return 1
         else:
             return 0
+        
+    @staticmethod
+    def visitar_no(no: int):
+        print(f"Estamos visitando o nó {no}")
 
+    @staticmethod
+    def marcar_no(marcados, no):
+        marcados.append(no)
+        return marcados
+        
+    def no_adjacente(self, no, marcados):
+        adjs = self.adj[no]
+        for _ in range(self.n):
+            if (adjs[_] != 0 and adjs[_] != math.inf) and _ not in marcados:
+                return _
+        return -1
+
+    def nos_adjacentes(self, no, marcados):
+        return [index for index, valor in enumerate(self.adj[no]) if (valor != 0 and valor != math.inf) and marcados]
+    
+    def is_adjac(self, i, j) -> int:
+        if (self.adj[i][j] != 0 or self.adj[j][i] != 0) == True:
+            return True
+
+    def is_adjacto(self, i):
+        vertices = []
+        for x in range(self.n):
+            if self.adj[i][x] or self.adj[x][i] > 0:
+                vertices.append(x)
+        return(vertices)
+    
+    def percurso_profundidade(self, v_inicio):
+        marcados = []
+        p = Pilha()
+        self.visitar_no(v_inicio)
+        marcados = self.marcar_no(marcados, v_inicio)
+        p.push(v_inicio)
+        while not p.is_empty():
+            no_atual = p.pop()
+            no_seguinte = self.no_adjacente(no_atual, marcados)
+            while no_seguinte != -1:
+                print(no_seguinte)
+                self.visitar_no(no_seguinte)
+                p.push(no_atual)
+                self.marcar_no(marcados, no_seguinte)
+                no_atual = no_seguinte
+                no_seguinte = self.no_adjacente(no_seguinte, marcados)
+             
+    def ordenacao_topologica(self, vInicio):
+        visitados = []
+        ge = [None]*self.n
+        pos = 0
+        for i in range(self.n):
+            ge[pos] = self.in_degree(i)
+            pos += 1
+        f = Fila()
+        for i in range(len(ge)):
+            if ge[i] == 0:
+                f.enqueue(i)
+                ge[i] = -1
+        while f.is_empty() == False:
+            n = f.dequeue()
+            self.visitar_no(n)
+            visitados.append(n)
+            j = self.is_adjacto(n)
+            for i in j:
+                if ge[i] == -1:
+                    continue
+                ge[i] -= 1
+            for i in range(len(ge)):
+                if ge[i] == 0:
+                    f.enqueue(i)
+                    ge[i] = -1
+        return visitados
+            
     def dijkstra(self, graph, start_vertex):#############################
-      D = {n:float('inf') for n in range(graph.n)}
-      D[start_vertex] = 0
-  
-      pq = PriorityQueue()
-      pq.put((0, start_vertex))
-  
-      while not pq.empty():
-          (dist, current_vertex) = pq.get()
-          graph.adj.append(current_vertex)
-  
-          for neighbor in range(graph.n):
-              if graph.adj[current_vertex][neighbor] != -1:
-                  distance = graph.adj[current_vertex][neighbor]
-                  if neighbor not in graph.visited:
-                      old_cost = D[neighbor]
-                      new_cost = D[current_vertex] + distance
-                      if new_cost < old_cost:
-                          pq.put((new_cost, neighbor))
-                          D[neighbor] = new_cost
-      print (D)
+        D = {n:float('inf') for n in range(graph.n)}
+        D[start_vertex] = 0
+        pq = PriorityQueue()
+        pq.put((0, start_vertex))
+        while not pq.empty():
+            (dist, current_vertex) = pq.get()
+            graph.adj.append(current_vertex)
+            for neighbor in range(graph.n):
+                if graph.adj[current_vertex][neighbor] != -1:
+                    distance = graph.adj[current_vertex][neighbor]
+                    if neighbor not in graph.visited:
+                        old_cost = D[neighbor]
+                        new_cost = D[current_vertex] + distance
+                        if new_cost < old_cost:
+                            pq.put((new_cost, neighbor))
+                            D[neighbor] = new_cost
+        print (D)
